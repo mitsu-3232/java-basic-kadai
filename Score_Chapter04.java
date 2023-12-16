97c08d5835a5296fb8b9b528768910e32e9ef275
mitsu-3232 <149814006+mitsu-3232@users.noreply.github.com>
2023-12-17 01:37:25 +0900

    kadai_004 をアップロード

---
+++ Score_Chapter04.java
@@ -0,0 +1,30 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Aさんの点数は" + 10 + "点");
+		System.out.println("Bさんの点数は" + 20 + "点");
+		System.out.println("Cさんの点数は" + 30 + "点");
+		System.out.println("Dさんの点数は" + 40 + "点");
+		System.out.println("Eさんの点数は" + 50 + "点");
+		System.out.println("Fさんの点数は" + 60 + "点");
+		System.out.println("Gさんの点数は" + 70 + "点");
+		System.out.println("Hさんの点数は" + 80 + "点");
+		System.out.println("Iさんの点数は" + 90 + "点");
+		System.out.println("Jさんの点数は" + 100 + "点");
+	
+		int[] score = {10,20,30,40,50,60,70,80,90,100};
+		
+		int sum = 0;
+		
+		for(int i = 0; i < score.length; i++) {
+
+		   sum += score[i]; //scoreの合計
+		}
+		
+		System.out.println("テストの平均点は" + (int)sum/score.length + "点");
+		}
+	}
+	
